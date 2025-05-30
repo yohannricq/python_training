b05101d2da7bef065612392a009fb2b9abf17252
yohannricq <yohann.ricq@gmail.com>
2025-05-30 17:51:05 +0200

    ajout de fonctions

--- console_app/main.py
+++ console_app/main.py
@@ -1,27 +1,33 @@
-prenomOK = False
-ageOK = False
-
-while not prenomOK:
-    prenom = input("Prénom : ")
+def ask_firstname():
+    prenomOK = False
+    while not prenomOK:
+        prenom = input("Prénom : ")
     
-    if prenom.strip() == "":
-        print("Erreur : Le prénom ne doit pas être vide")
-    else:
-        prenomOK = True
-        
-while not ageOK:
-    age = input("Age : ")
+        if prenom.strip() == "":
+            print("Erreur : Le prénom ne doit pas être vide")
+        else:
+            prenomOK = True
+            
+    return prenom
 
-    try:
-        age_prochain = int(age) + 1
-    except ValueError:
-        print("Erreur : Vous devez saisir un nombre pour l'age")
-    else:
-        ageOK = True
+def ask_age():
+    ageOK = False
+    while not ageOK:
+        age_str = input("Age : ")
+
+        try:
+            age = int(age_str)
+        except ValueError:
+            print("Erreur : Vous devez saisir un nombre pour l'age")
+        else:
+            ageOK = True
+            
+    return age
+
+prenom = ask_firstname()
+age = ask_age()
+
+age_prochain = int(age) + 1
         
-# print(type(prenom))
-# print(type(age))
-        
-# print("Hello " + prenom + ", tu as " + age + " ans !")
 print(f"Hello {prenom}, tu as {age} ans !")
-print("L'an prochain tu auras " + str(age_prochain) + " ans !")
+print(f"L'an prochain tu auras {age_prochain} ans")
