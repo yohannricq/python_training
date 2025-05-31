318e71246406bca65ea2a48b888fbafc9bd9105a
yohannricq <yohann.ricq@gmail.com>
2025-05-31 20:00:06 +0200

    ajout des infos(majeur, mineur...) de la personne

--- console_app/main.py
+++ console_app/main.py
@@ -10,10 +10,10 @@ def ask_firstname():
             
     return prenom
 
-def ask_age():
+def ask_age(prenom):
     ageOK = False
     while not ageOK:
-        age_str = input("Age : ")
+        age_str = input(f"Age de {prenom} : ")
 
         try:
             age = int(age_str)
@@ -24,10 +24,45 @@ def ask_age():
             
     return age
 
-prenom = ask_firstname()
-age = ask_age()
-
-age_prochain = int(age) + 1
+def showPersonInfos(prenom, age):
+    print(f"Hello {prenom}, tu as {age} ans !")
+    
+    # infos_majeur = ""
+    # infos_mineur = ""
+    
+    # if age == 17:
+    #     print("Tu es presque majeur")
+    # elif age == 18:
+    #     print("Tu es tout juste majeur")
+    # elif age > 18:
+    #     infos_majeur = "Tu es majeur"
+    #     if age > 60:
+    #         infos_majeur += " et tu es un sénior"
+    #     print(infos_majeur)
+    # else:
+    #     infos_mineur = "Tu es mineur"
+    #     if age < 10:
+    #         infos_mineur += " et tu es un enfant"
+    #     print(infos_mineur)
         
-print(f"Hello {prenom}, tu as {age} ans !")
-print(f"L'an prochain tu auras {age_prochain} ans")
+    if age == 17:
+        print("Tu es presque majeur")
+    elif age == 18:
+        print("Tu es tout juste majeur")
+    elif age > 60:
+        print("Tu es un sénior")
+    elif age > 18:
+        print("Tu es majeur")
+    elif age < 10:
+        print("Tu es un enfant")
+    else:
+        print("Tu es mineur")
+
+prenom1 = ask_firstname()
+prenom2 = ask_firstname()
+
+age1 = ask_age(prenom1)
+age2 = ask_age(prenom2)
+        
+showPersonInfos(prenom1, age1)
+showPersonInfos(prenom2, age2)
